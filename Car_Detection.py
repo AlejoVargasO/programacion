2847e36e7455f4bb78a88548a71c70af0aecdb25
Alejandro <alvargaso@unal.edu.co>
2024-09-23 17:02:49 -0500

    Codigo utilizado

---
+++ Car_Detection.py
@@ -0,0 +1,36 @@
+import cv2
+import jetson.inference
+import jetson.utils
+import numpy as np
+
+net = jetson.inference.detectNet("ssd-mobilenet-v2",["--model=/my_project/ssd-mobilenet.onnx","--labels=/my_project/labels.txt","--input-blob=input_0","--output-cvg=scores","--output-bbox=boxes"])
+
+#camera = cv2.VideoCapture("/dev/video0") # Este es el puerto de la cámara
+camera = cv2.VideoCapture('videoprueba.mp4') #utilizamos la ruta del Video para la inferencia.
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+camera.set(cv2.CAP_PROP_FRAME_WIDTH,480)
+
+#Entramos en un bucle para la ejecución de la inferencia
+while True:
+	r,frame = camera.read()
+	img = jetson.utils.cudaFromNumpy(cv2.cvtColor(frame,cv2.COLOR_BGR2RGBA).astype(np.float))
+	
+	detections = net.Detect(img)
+	n_obj = len(detections)
+	for detect in detections:
+		Id = detect.ClassID
+		item = net.GetClassDesc(Id)
+		
+		if item == "FCar": color = (0,255,0) #Cando se detecte un carro se pintará de verde
+		else: color = (0,0,255)
+
+		cv2.putText(frame,item,(int(detect.Left),int(detect.Top)),cv2.FONT_HERSHEY_SIMPLEX,1,color,3)
+		cv2.rectangle(frame,(int(detect.Left),int(detect.Top)),(int(detect.Right),int(detect.Bottom)),(255,0,0),1)
+
+	cv2.imshow("window",frame)
+	if cv2.waitKey(1) == ord("q"):
+		break;
+
+
+camera.release()
+cv2.destroyAllWindows()
